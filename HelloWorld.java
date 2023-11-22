957f97d6bba3e2d51b9f92b30a7ab5dcc3c7406b
skalashettidevops <150411969+skalashettidevops@users.noreply.github.com>
2023-11-22 12:54:27 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
      System.out.println("Hello world!");
      System.out.println("Hello world!");
      System.out.println("Hello world!");
-     System.out.println("Hello world!");
+     
   
   }
 }
